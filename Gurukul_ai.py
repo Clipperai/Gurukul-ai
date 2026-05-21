ea09e295a2aa3f208f42831fced05e46722d15a7
Nishant Chauhan <166284550+Clipperai@users.noreply.github.com>
2026-05-21 21:59:07 +0530

    Enhance greeting message in Gurukul AI
    Updated greeting message in the toast notification.

--- Gurukul_ai.py
+++ Gurukul_ai.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title = 'Gurukul AI')
 
 st.title("Gurukul AI - A 24/7 AI Study Companion")
 
-st.toast("Hello, Builde 👋")
+st.toast("Hello, Builde 👋\n How can I help you today?")
 
 st.write("Go to Pages from sidebar")
 st.toast("Go to Pages from sidebar")
