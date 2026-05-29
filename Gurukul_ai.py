33f64ded4af23fd7e070285c91349b19fa5cb4b6
Nishant Chauhan <166284550+Clipperai@users.noreply.github.com>
2026-05-29 20:56:41 +0530

    Update Gurukul_ai.py

--- Gurukul_ai.py
+++ Gurukul_ai.py
@@ -2,7 +2,11 @@ import streamlit as st
 from brain import ask_ai
 
 
-st.set_page_config(page_title = 'Gurukul AI')
+st.set_page_config(
+    page_title = 'Gurukul AI',
+    page_icon = '🛕',
+    layout = 'centered
+)
 
 st.title("Gurukul AI - A 24/7 AI Study Companion")
 
