2fe9652b0778540889b56a1a3e30985e8dce72f6
Nishant Chauhan <166284550+Clipperai@users.noreply.github.com>
2026-05-30 17:46:21 +0530

    Update Gurukul_ai.py

--- Gurukul_ai.py
+++ Gurukul_ai.py
@@ -1,23 +1,12 @@
 import streamlit as st
 from brain import ask_ai
 
-
-st.set_page_config(
-    page_title = 'Gurukul AI',
-    page_icon = '🛕',
-    layout = 'centered'
-)
-
 st.title("Gurukul AI - A 24/7 AI Study Companion")
 
 st.header("Chat with Gurukul AI")
 st.write("Your Personalized AI Guru")
-st.toast("Go to Pages from sidebar")
 
 st.space("small")
-
-st.toast("How can I help you todaay?")
-st.toast("Hello, Builder!👋")
 
 query = st.chat_input("ask anything")
 
